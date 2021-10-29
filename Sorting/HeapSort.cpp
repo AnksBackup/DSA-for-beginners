38c56985211bf1f77354bc20bd991d82bddcb8bb
Swagata <swagatasrimani1999@gmail.com>
2021-10-29 22:29:09 +0530

    added sorting programs

---
+++ Sorting/HeapSort.cpp
@@ -0,0 +1,18 @@
+void heapify(int arr[], int n, int i) {
+  // Find largest among root, left child and right child
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest])
+    largest = left;
+
+  if (right < n && arr[right] > arr[largest])
+    largest = right;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+  }
+}
