1b19a9e36ed194b0b03656f734055733e1ca6e77
Kavya-Shekar <44774805+Kavya-Shekar@users.noreply.github.com>
2021-10-31 23:28:10 +0530

    Update HeapSort.cpp

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -1,3 +1,12 @@
+// C++ program for implementation of Heap Sort
+
+// Time complexity: O(nlogn)
+
+#include <iostream> 
+using namespace std;
+
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
 void heapify(int arr[], int n, int i) {
   // Find largest among root, left child and right child
   int largest = i;
@@ -12,7 +21,44 @@ void heapify(int arr[], int n, int i) {
 
     // Swap and continue heapifying if root is not largest
     if (largest != i) {
-      swap(&arr[i], &arr[largest]);
+      swap(arr[i], arr[largest]);
       heapify(arr, n, largest);
   }
+} 
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+ 
+    // One by one extract an element from heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+ 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+ 
+// A utility function to print array of size n
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+ 
+// Driver code
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6, 7 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    heapSort(arr, n);
+ 
+    cout << "Sorted array is: ";
+    printArray(arr, n);
 }
